91759087d4f21bdab1236b711cac2c229d2442bf
Sebastian Pipping <webmaster@hartwork.org>
2008-01-23 17:21:58 +0000

    Cosmetics

--- make_smaller_binaries.cmd
+++ make_smaller_binaries.cmd
@@ -5,3 +5,7 @@ ECHO Compressing Irssi EXEs and DLLs with UPX...
 REM NOTE: cthelper.exe must stay uncompressed!
 FOR /r %SRC_DIR%\bin %%e IN (irssi.exe,puttycyg.exe,*.dll) DO upx "%%e" --best --compress-icons=0 --nrv2e --crp-ms=999999
 FOR /r %SRC_DIR%\lib %%e IN (*.exe,*.dll) DO upx "%%e" --best --compress-icons=0 --nrv2e --crp-ms=999999
+
+ECHO Done.
+ECHO.
+PAUSE
